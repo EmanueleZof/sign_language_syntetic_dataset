5a2f5414648435a178a7f4b8fd0adf4fd210d84f
Emanuele Zof <zof.emanuele@gmail.com>
2025-03-21 21:33:29 +0100

    Bug fix

--- script/mimic_motion_install.sh
+++ script/mimic_motion_install.sh
@@ -3,30 +3,14 @@
 # https://github.com/Tencent/MimicMotion
 ################################################################################
 
-echo "Installing dependencies"
+echo "***** Installing MimicMotion *****"
 
-#---- Dependencies ----#
-av12()
-{
-    pip install av==12.0.0
-}
+#---- libraries ----#
+pip install decord onnxruntime omegaconf
 
-av13()
-{
+if [ $1 -eq "av13" ]
+then
     pip install av==13.1.0
-}
-
-dependencies()
-{
-    pip install decord onnxruntime omegaconf
-
-    if [[ $1 == "av13" ]]; then
-        pip install av==13.1.0
-    else
-        pip install av==12.0.0
-    fi
-}
-
-#dependencies
-
-echo "$0" "$1"
+else
+    pip install av==12.0.0
+fi
