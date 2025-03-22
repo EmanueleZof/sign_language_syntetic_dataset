4b1c1abd4a63927a1ef2c110a12b99d4630bdef5
Emanuele Zof <zof.emanuele@gmail.com>
2025-03-22 16:08:27 +0100

    add args

--- inference.py
+++ inference.py
@@ -23,4 +23,14 @@ from MimicMotion.mimicmotion.utils.loader import create_pipeline
 from MimicMotion.mimicmotion.utils.utils import save_to_mp4
 from MimicMotion.mimicmotion.dwpose.preprocess import get_video_pose, get_image_pose
 
-print('Test1');
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#
+parser = argparse.ArgumentParser()
+parser.add_argument("--log_file", type=str, default=None)
+parser.add_argument("--inference_config", type=str, default="configs/test.yaml") #ToDo
+parser.add_argument("--output_dir", type=str, default="outputs/", help="path to output")
+parser.add_argument("--no_use_float16", action="store_true", help="Whether use float16 to speed up inference")
+args = parser.parse_args()
+
+print(args);
