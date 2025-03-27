126ad0d262fb1f172af37db4f3722cfc7a786613
Emanuele Zof <zof.emanuele@gmail.com>
2025-03-27 22:06:33 +0100

    Output info for install script

--- scripts/mimic_motion_install.sh
+++ scripts/mimic_motion_install.sh
@@ -3,9 +3,15 @@
 # https://github.com/Tencent/MimicMotion
 ################################################################################
 
-echo "***** Installing MimicMotion *****"
+echo
+echo "***** MimicMotion *****"
+echo
 
 #---- Install libraries ----#
+echo
+echo "** Install libraries **"
+echo
+
 pip install decord onnxruntime omegaconf
 
 if [[ $1 == av13 ]]
@@ -16,15 +22,26 @@ else
 fi
 
 #---- Clone Git repository ----#
+echo
+echo "** Clone Git repository **"
+echo
+
 cd sign_language_syntetic_dataset/
 git clone https://github.com/Tencent/MimicMotion.git
 
 #---- Create folders ----#
-#cd MimicMotion/
+echo
+echo "** Create folders **"
+echo
+
 mkdir models
 mkdir -p models/DWPose
 
 #---- Load models ----#
+echo
+echo "** Load models **"
+echo
+
 wget https://huggingface.co/yzd-v/DWPose/resolve/main/yolox_l.onnx?download=true -O models/DWPose/yolox_l.onnx
 
 wget https://huggingface.co/yzd-v/DWPose/resolve/main/dw-ll_ucoco_384.onnx?download=true -O models/DWPose/dw-ll_ucoco_384.onnx
