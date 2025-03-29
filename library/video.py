bf2bab229e10e70484b00a02a196e1562910e40e
Emanuele Zof <zof.emanuele@gmail.com>
2025-03-29 15:14:25 +0100

    Add functions

--- library/video.py
+++ library/video.py
@@ -1,3 +1,4 @@
+import random
 import pandas as pd
 
 class Video:
@@ -11,7 +12,7 @@ class Video:
       self.path = path
       self.data_type = data_type
       self.classes = classes
-      self.dataset = {}
+      self.dataset = []
 
       self.main()
 
@@ -22,21 +23,26 @@ class Video:
         df_classes = pd.read_csv(classes_path)
         df_test_labels = pd.read_csv(data_path, header=None)
 
-        obj = []
-
-        for dataset_class in classes:
-            hi_class_id = df_classes.loc[df_classes["EN"] == dataset_class].iloc[0]["ClassId"]
+        for class_name in classes:
+            hi_class_id = df_classes.loc[df_classes["EN"] == class_name].iloc[0]["ClassId"]
             hi_videos = df_test_labels.loc[df_test_labels[1] == hi_class_id][0].tolist()
-            obj.append({
+            hi_videos =[f"{main_folder_path}/{data_type}/color/{path}_color.mp4" for path in hi_videos]
+
+            self.dataset.append({
+                "class_name": class_name,
                 "class_id": hi_class_id,
                 "video_list": hi_videos
             })
-        
-        return obj
-
 
     def main(self):
       if (self.loader == "autsl" and self.path != "") :
-        url_list = self._autsl_loader(self.path, self.data_type, self.classes)
+        self._autsl_loader(self.path, self.data_type, self.classes)
       else :
         print("Nessun loader o path specificato")
+
+    def get_dataset(self):
+      return self.dataset
+
+    def show_random_video(self, class_name):
+      selected_class = [e for e in self.dataset if e["class_name"] == class_name]
+      return random.choice(selected_class[0]["video_list"])
