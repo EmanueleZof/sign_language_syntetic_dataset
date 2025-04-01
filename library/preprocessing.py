9775a14395890575e5b1dedbf50bdb778238057d
Emanuele Zof <zof.emanuele@gmail.com>
2025-04-01 21:12:36 +0200

    Add draw frames

--- library/preprocessing.py
+++ library/preprocessing.py
@@ -14,8 +14,31 @@ class Preprocessor:
 
         self.output_dir = "."
         
+def _draw_landmarks(self, image, results):
+    self.mp_drawing.draw_landmarks( image, 
+                                    results.face_landmarks, 
+                                    mp_holistic.FACEMESH_TESSELATION, 
+                                    mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                                    mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
 
-def _process_video(self, video):
+    self.mp_drawing.draw_landmarks( image, 
+                                    results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                    mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                    mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2))
+
+    self.mp_drawing.draw_landmarks( image, 
+                                    results.left_hand_landmarks, 
+                                    mp_holistic.HAND_CONNECTIONS, 
+                                    mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                    mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2))
+
+    self.mp_drawing.draw_landmarks( image, 
+                                    results.pose_landmarks, 
+                                    mp_holistic.POSE_CONNECTIONS, 
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+
+def _process_video(self, video, show=False):
     cap = cv2.VideoCapture(video)
 
     if (cap.isOpened() == False):
@@ -28,15 +51,25 @@ def _process_video(self, video):
             if ret == False:
                 frame = np.zeros((512, 512, 3), np.uint8)
             
-            image, results = detect_landmarks(frame, holistic)
-            #_draw_landmarks(image, results)
-            keypoints = extract_keypoints(results)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            results = holistic.process(image)
+
+            image.flags.writeable = True   
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if (show == True):
+                _draw_landmarks(image, results)
+                cv2_imshow(image)
+
+            #keypoints = extract_keypoints(results)
 
             #utils.create_folder(save_dir)
             #file_name = os.path.join(save_dir, str(frame_num))
             #np.save(file_name, keypoints)
 
             #cv2.imshow(image)
-            cv2_imshow(image)
+            
         cap.release()
         cv2.destroyAllWindows()
