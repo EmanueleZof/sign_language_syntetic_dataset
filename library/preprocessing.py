936c6a5500567cab3abdc32fa37756917b4ed7fb
Emanuele Zof <zof.emanuele@gmail.com>
2025-04-01 21:26:14 +0200

    Bug fix

--- library/preprocessing.py
+++ library/preprocessing.py
@@ -15,26 +15,27 @@ class Preprocessor:
     def _draw_landmarks(self, image, results):
         self.mp_drawing.draw_landmarks( image, 
                                         results.face_landmarks, 
-                                        mp_holistic.FACEMESH_TESSELATION, 
-                                        mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-                                        mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+                                        self.mp_holistic.FACEMESH_TESSELATION, 
+                                        self.mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                                        self.mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
 
         self.mp_drawing.draw_landmarks( image, 
-                                        results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                        mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-                                        mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2))
+                                        results.right_hand_landmarks, 
+                                        self.mp_holistic.HAND_CONNECTIONS, 
+                                        self.mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                        self.mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2))
 
         self.mp_drawing.draw_landmarks( image, 
                                         results.left_hand_landmarks, 
-                                        mp_holistic.HAND_CONNECTIONS, 
-                                        mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                                        mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2))
+                                        self.mp_holistic.HAND_CONNECTIONS, 
+                                        self.mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                        self.mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2))
 
         self.mp_drawing.draw_landmarks( image, 
                                         results.pose_landmarks, 
-                                        mp_holistic.POSE_CONNECTIONS, 
-                                        mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                        mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+                                        self.mp_holistic.POSE_CONNECTIONS, 
+                                        self.mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                        self.mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
 
     def _process_video(self, video, show=False):
         cap = cv2.VideoCapture(video)
